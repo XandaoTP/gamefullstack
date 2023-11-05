c1d2072e8b3564536d286273933a5b760a9d1367
unknown <alexandre8603@gmail.com>
2023-11-04 21:23:00 -0300

    init server

--- JogoMongo/src/server/index.ts
+++ JogoMongo/src/server/index.ts
@@ -3,7 +3,14 @@ import * as dotenv from 'dotenv';
 import { createExpressServer } from "routing-controllers";
 dotenv.config()
 
+const port = process.env.PORT
+const host = process.env.HOST
+
+
 createExpressServer({
    controllers: [],
    cors: true
+}).listen(port, host, () => {
+    console.log(`Servidor iniciado em http://${host}:${port}`);
+    
 })
